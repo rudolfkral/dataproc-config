7c9170a10a18e4331a9816a928144d58f875b061
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2016-03-02 11:50:23 +0100

    Add jupyter notebook config and initialization

---
+++ scripts/03_jupyter_config.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+
+# Only run on the master node
+ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
+if [[ "${ROLE}" == 'Master' ]]; then
+	# Install iPython Notebook and create a profile
+	cd
+	mkdir IPythonNB
+	cd IPythonNB
+	ipython profile create default
+	
+	# Set up configuration for iPython Notebook
+	echo "c = get_config()" >  /root/.ipython/profile_default/ipython_notebook_config.py
+	echo "c.NotebookApp.ip = '*'" >>  /root/.ipython/profile_default/ipython_notebook_config.py
+	echo "c.NotebookApp.open_browser = False"  >>  /root/.ipython/profile_default/ipython_notebook_config.py
+	echo "c.NotebookApp.port = 8123" >>  /root/.ipython/profile_default/ipython_notebook_config.py
+	
+	# Setup script for iPython Notebook so it uses the cluster's Spark
+	cat > /root/.ipython/profile_default/startup/00-pyspark-setup.py <<'_EOF'
+import os
+import sys
+spark_home = '/usr/lib/spark/'
+os.environ["SPARK_HOME"] = spark_home
+sys.path.insert(0, os.path.join(spark_home, 'python'))
+sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.9-src.zip'))
+exec(compile(open(os.path.join(spark_home, 'python/pyspark/shell.py')).read(), os.path.join(spark_home, 'python/pyspark/shell.py'), 'exec'))
+_EOF
+	
+	# Start iPython Notebook on port 8123
+	nohup ipython notebook --no-browser --ip=* --port=8123 > /var/log/python_notebook.log &
+fi
