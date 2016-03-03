8b07f425dd737e031bc25eb5c4b2974edee1200f
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2016-03-03 15:09:45 +0100

    Update elasticsearch-hadoop

--- scripts/001_jars.sh
+++ scripts/001_jars.sh
@@ -1,1 +1,1 @@
-gsutil cp gs://ai-bucket/dataproc/jars/elasticsearch-hadoop-2.2.0.jar /usr/lib/hadoop/lib/
+gsutil cp gs://ai-bucket/dataproc/jars/elasticsearch-hadoop-2.3.0.BUILD-20160303.040409-29.jar /usr/lib/hadoop/lib/
