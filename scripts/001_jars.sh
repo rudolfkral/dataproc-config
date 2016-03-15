f115f52bc04cde48807ddeeb0391e6bced9f9483
Szymon Matejczyk <smatejczyk@gmail.com>
2016-03-15 16:56:46 +0100

    added postgres jar

--- scripts/001_jars.sh
+++ scripts/001_jars.sh
@@ -1,1 +1,2 @@
 gsutil cp gs://ai-bucket/dataproc/jars/elasticsearch-hadoop-2.3.0.BUILD-20160303.040409-29.jar /usr/lib/hadoop/lib/
+wget https://jdbc.postgresql.org/download/postgresql-9.4.1208.jar -P /usr/lib/hadoop/lib/
