641d9ccc929546482fb1518e9b627b77bea0eef0
Szymon Matejczyk <smatejczyk@gmail.com>
2016-04-20 12:03:14 +0200

    avro dependency

--- scripts/001_jars.sh
+++ scripts/001_jars.sh
@@ -1,3 +1,4 @@
 gsutil cp gs://ai-bucket/dataproc/jars/elasticsearch-hadoop-2.3.0.BUILD-20160303.040409-29.jar /usr/lib/hadoop/lib/
 wget https://jdbc.postgresql.org/download/postgresql-9.4.1208.jar -P /usr/lib/hadoop/lib/
 wget http://central.maven.org/maven2/com/databricks/spark-avro_2.10/2.0.1/spark-avro_2.10-2.0.1.jar -P /usr/lib/hadoop/lib/
+wget http://central.maven.org/maven2/org/apache/avro/avro-mapred/1.7.7/avro-mapred-1.7.7.jar -P /usr/lib/hadoop/lib/
