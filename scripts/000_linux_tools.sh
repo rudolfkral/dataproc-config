04659ba52c6159402063ec3afa3070e49d3b3845
Szymon Matejczyk <smatejczyk@gmail.com>
2016-10-13 09:18:29 +0200

    new version sqlalchemy

--- scripts/000_linux_tools.sh
+++ scripts/000_linux_tools.sh
@@ -1,2 +1,3 @@
 apt-get update
-apt-get -y install vim screen zsh libxml2-dev libxslt1-dev htop mc libpq-dev libhdf5-serial-dev
+apt-get -y install vim screen zsh libxml2-dev libxslt1-dev htop mc libpq-dev \
+        libhdf5-serial-dev libmysqlclient-dev
