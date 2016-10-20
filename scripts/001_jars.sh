02720a6b97f26ffb57810168f2988d4922ef6e65
Szymon Matejczyk <smatejczyk@gmail.com>
2016-10-20 11:22:20 +0200

    added spark-bigquery and spark-avro jars

--- scripts/001_jars.sh
+++ scripts/001_jars.sh
@@ -1,2 +1,4 @@
 gsutil cp gs://ai-bucket/dataproc/jars/elasticsearch-hadoop-2.3.0.BUILD-20160303.040409-29.jar /usr/lib/hadoop/lib/
 wget https://jdbc.postgresql.org/download/postgresql-9.4.1208.jar -P /usr/lib/hadoop/lib/
+wget https://dl.bintray.com/spark-packages/maven/spotify/spark-bigquery/0.1.2-s_2.10/:spark-bigquery-0.1.2-s_2.10.jar -P /usr/lib/hadoop/lib/
+wget https://dl.bintray.com/spark-packages/maven/databricks/spark-avro/3.0.1-s_2.10/:spark-avro-3.0.1-s_2.10.jar -P /usr/lib/hadoop/lib/
