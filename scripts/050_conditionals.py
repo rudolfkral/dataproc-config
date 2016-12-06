7d88763b87810896aa3c5647d03c20f0aa63c9bb
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2016-12-06 14:17:20 +0100

    Fix indendation

--- scripts/050_conditionals.py
+++ scripts/050_conditionals.py
@@ -1,23 +1,25 @@
 import subprocess
 
-def get_tags():
-	try:
-		p = subprocess.Popen(['/usr/share/google/get_metadata_value','tags'], 
-					stdout=subprocess.PIPE, 
-					stderr=subprocess.PIPE)
-		out, err = p.communicate()
-		tags = eval(out.decode("utf-8"))
-	except:
-		tags = []
 
-	return tags
+def get_tags():
+    try:
+        p = subprocess.Popen(['/usr/share/google/get_metadata_value', 'tags'],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        tags = eval(out.decode("utf-8"))
+    except:
+        tags = []
+
+    return tags
+
 
 if __name__ == '__main__':
-	tags = get_tags()	
+    tags = get_tags()
 
-	print("Found tags", tags)
+    print("Found tags", tags)
 
-	if 'spacy-download' in tags:
-		print("Downloading spacy models")
-	    from spacy.en.download import main
-	    main()
+    if 'spacy-download' in tags:
+        print("Downloading spacy models")
+        from spacy.en.download import main
+        main()
